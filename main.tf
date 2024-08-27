bee8240a8f7442db04f38a0b03d5e9d9d248d640
Opeyemi Fasina <96543598+xyemi@users.noreply.github.com>
2024-08-27 10:29:39 -0600

    loading balancer

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
-  vpc_id  = "module.blog_vpc.vpc_id
+  vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
